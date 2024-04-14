3f14b0eaab8d3886d5fe4c652a05725c97c05add
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-04-14 03:12:00 -0700

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -1,5 +1,5 @@
 from bot import Bot
-from pyrogram.types import Message
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram import filters
 from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT
 from datetime import datetime
@@ -12,9 +12,8 @@ async def stats(bot: Bot, message: Message):
     time = get_readable_time(delta.seconds)
     await message.reply(BOT_STATS_TEXT.format(uptime=time))
 
-
 @Bot.on_message(filters.private & filters.incoming)
-async def useless(_, message):
+async def useless(_, message: Message):
     if USER_REPLY_TEXT in message.text:
         keyboard = InlineKeyboardMarkup(
             [
@@ -23,19 +22,3 @@ async def useless(_, message):
         )
         await message.reply_text(USER_REPLY_TEXT, reply_markup=keyboard)
 
-'''
-USER_REPLY_TEXT = "❌ Don't send me messages directly I'm only File Share bot!"
-URL_BUTTON_TEXT = "Visit Website"
-URL_BUTTON_URL = "https://example.com"
-
-@Bot.on(events.NewMessage)
-async def handle_message(event):
-    message = event.message
-    if message.is_private and USER_REPLY_TEXT in message.text:
-        # Create an inline keyboard with a URL button
-        button = types.BotInlineKeyboardButton(text=URL_BUTTON_TEXT, url=URL_BUTTON_URL)
-        keyboard = types.InlineKeyboardMarkup([[button]])
-        
-        # Reply to the user with the predefined message and the URL button
-        await message.reply(USER_REPLY_TEXT, buttons=keyboard)
-'''
