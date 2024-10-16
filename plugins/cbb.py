18db6fdceda24af2366fb3a8c4122d482af3bcdf
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-10-16 19:34:20 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -16,7 +16,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
                 [
                     [
                         InlineKeyboardButton("⚡️ ᴄʟᴏsᴇ", callback_data = "close"),
-                        InlineKeyboardButton('🍁 DeV', url='https://t.me/')
+                        InlineKeyboardButton('🍁 DeV', url='https://t.me/ultroidxTeam')
                     ]
                 ]
             )
