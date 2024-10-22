825acb04ad43ea6a996748f4ce9728cfa855f142
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-10-22 23:54:02 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,29 +1,48 @@
 # btn : about and close change here
 
-from pyrogram import __version__
+#from pyrogram import __version__
 from bot import Bot
-from config import OWNER_ID
+#from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
+# https://t.me/Ultroid_Official/524
+
+from pyrogram import __version__, Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram.enums import ParseMode
+#from database.database import full_userbase
+#from bot import Bot
+from config import OWNER_ID, ADMINS, CHANNEL, SUPPORT_GROUP, OWNER
+#from plugins.cmd import *
+
+# Callback query handler
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
+
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ ᴏᴡɴᴇʀ : <a href='tg://user?id={OWNER_ID}'>ᴍɪᴋᴇʏ</a>\n○ ᴍʏ ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/chrunchyrool'>Channel</a>\n○ ᴍᴏᴠɪᴇs ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/Ongoing_Movies_Series'>Ongoing</a>\n○ ᴏᴜʀ ᴄᴏᴍᴍᴜɴɪᴛʏ : <a href='https://t.me/Ongoingmoviehub'>Chat</a></b>",
-            disable_web_page_preview = True,
-            reply_markup = InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton("⚡️ ᴄʟᴏsᴇ", callback_data = "close"),
-                        InlineKeyboardButton('🍁 DeV', url='https://t.me/ultroidxTeam')
-                    ]
-                ]
+            text=f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n"
+                 f"○ Language : <code>Python3</code>\n"
+                 f"○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n"
+                 f"○ Source Code : <a href='https://youtu.be/BeNBEYc-q7Y'>Click here</a>\n"
+                 f"○ Channel : @{CHANNEL}\n"
+                 f"○ Support Group : @{SUPPORT_GROUP}</b>",
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("🔒 Close", callback_data="close")]]
             )
         )
     elif data == "close":
         await query.message.delete()
         try:
             await query.message.reply_to_message.delete()
-        except:
-            pass
+        except Exception as e:
+            print(f"Error deleting reply-to message: {e}")
+
+# https://t.me/Ultroid_Official/524
+
+
+# ultroidofficial : YT
+
+
