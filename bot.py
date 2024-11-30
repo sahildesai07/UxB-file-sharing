357426b2711d32d01387ed0d2ad5508e2673802b
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-11-29 20:04:43 -0800

    Update bot.py
    fixed error channel id

--- bot.py
+++ bot.py
@@ -10,6 +10,10 @@ import sys
 from datetime import datetime
 
 from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCESUB_CHANNEL, FORCESUB_CHANNEL2, FORCESUB_CHANNEL3, CHANNEL_ID, PORT
+import pyrogram.utils
+
+pyrogram.utils.MIN_CHAT_ID = -999999999999
+pyrogram.utils.MIN_CHANNEL_ID = -100999999999999
 
 class Bot(Client):
     def __init__(self):
